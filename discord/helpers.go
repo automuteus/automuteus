7eaef2499d4a3ad055232c999c082c536c3ecdc7
Kyle Montag <thekylemontag@gmail.com>
2020-09-23 16:20:40 -0400

    Reset nicknames on `.au end`

--- discord/helpers.go
+++ discord/helpers.go
@@ -6,6 +6,49 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+func (guild *GuildState) resetTrackedMembers(dg *discordgo.Session) {
+
+	g := guild.verifyVoiceStateChanges(dg)
+
+	for _, voiceState := range g.VoiceStates {
+
+		guild.UserDataLock.RLock()
+		if userData, ok := guild.UserData[voiceState.UserID]; ok {
+
+			//only issue a change if the user isn't in the right state already
+			if !voiceState.Mute || !voiceState.Deaf {
+
+				//only issue the req to discord if we're not waiting on another one
+				if !userData.pendingVoiceUpdate {
+					guild.UserDataLock.RUnlock()
+					//wait until it goes through
+					userData.pendingVoiceUpdate = true
+
+					go guild.updateUserInMap(voiceState.UserID, userData)
+
+					go guildMemberReset(dg, guild.ID, userData.user)
+
+					updateMade = true
+					guild.UserDataLock.RLock()
+				}
+
+			}
+		} else { //the user doesn't exist in our userdata cache; add them
+			guild.UserDataLock.RUnlock()
+
+			guild.addFullUserToMap(g, voiceState.UserID)
+
+			guild.UserDataLock.RLock()
+
+		}
+		guild.UserDataLock.RUnlock()
+	}
+}
+
+func guildMemberReset(s *discordgo.Session, guildID string, user User) {
+	guildMemberUpdate(s, guildID, user.userID, false, false, user.originalNick)
+}
+
 func guildMemberUpdate(s *discordgo.Session, guildID string, userID string, mute bool, deaf bool, nick string) {
 	log.Printf("Issuing update request to discord for userID %s with mute=%v deaf=%v nick=%s\n", userID, mute, deaf, nick)
 	data := struct {
