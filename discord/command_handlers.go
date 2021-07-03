d4341245baf808a68141867ad91e960df574ac84
kurokobo <2920259+kurokobo@users.noreply.github.com>
2021-07-03 12:01:41 +0000

    fix: add expiration check for the invite command

--- discord/command_handlers.go
+++ discord/command_handlers.go
@@ -462,6 +462,9 @@ func (bot *Bot) HandleCommand(isAdmin, isPermissioned bool, sett *settings.Guild
 			if len(args[1:]) == 0 {
 				bot.GalactusClient.SendChannelMessageEmbed(m.ChannelID, premiumEmbedResponse(m.GuildID, tier, daysRem, sett))
 			} else {
+				if premium.IsExpired(tier, daysRem) {
+					tier = premium.FreeTier
+				}
 				arg := strings.ToLower(args[1])
 				if isAdmin {
 					if arg == "invite" || arg == "invites" || arg == "inv" {
